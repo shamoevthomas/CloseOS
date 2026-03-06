5db2515f2249f506c33255ea952c2d5f7463fd03
Thomas Shamoev <thomasshamoev@MacBook-Air-de-Thomas.local>
2026-03-06 17:22:08 +0100

    feat: add 2-day trial to Stripe checkout

--- api/checkout.ts
+++ api/checkout.ts
@@ -83,6 +83,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
             ...(finalReferral ? { referral_code: String(finalReferral) } : {}),
             ...(promotekitReferral ? { promotekit_referral: String(promotekitReferral) } : {}),
           },
+          trial_period_days: 2,
         },
       });
 
