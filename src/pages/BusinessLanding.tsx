d45dbbca1c6977bb7df44786ddb4d7c74aba3c70
Thomas Shamoev <thomasshamoev@MacBook-Air-de-Thomas.local>
2026-03-06 23:31:03 +0100

    feat: set business logos to h-21 (84px)

--- src/pages/BusinessLanding.tsx
+++ src/pages/BusinessLanding.tsx
@@ -24,7 +24,7 @@ export const BusinessLanding: React.FC = () => {
                     <div className="flex items-center gap-3">
                         <img
                             alt="CloseOS Logo"
-                            className="w-auto object-contain h-16"
+                            className="w-auto object-contain h-[84px]"
                             src="/CloseOS Buisness.png"
                         />
                     </div>
